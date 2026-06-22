a23ee520eabadd82b921f7f6c3138b4b897d1601
arsanantha1890-prog <arsanantha1890@gmail.com>
2026-06-22 17:16:37 +0530

    welcome email sent successfully

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,12 +1,13 @@
 
 import mongoose from "mongoose"
+import { ENV } from "./env.js";
 
 export const connectDB = async () => {
   try {
-    const { MONGO_URI } = process.env;
+    const { MONGO_URI } = ENV;
     if (!MONGO_URI) throw new Error("MONGO_URO is not set");
     
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(ENV.MONGO_URI);
     console.log("MONGODB CONNECTED:", conn.connection.host);
   } catch (error) {
     console.error("Error connection to MONGODB:",error);
@@ -21,7 +22,7 @@ export const connectDB = async () => {
   try {
     console.log("Trying to connect...");
 
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(ENV.MONGO_URI);
 
     console.log("MongoDB Connected!");
     console.log(conn.connection.host);
