8641c34b047e0ab0d2fa7d6eda7f0bfa8e6e9eb6
arsanantha1890-prog <arsanantha1890@gmail.com>
2026-07-14 16:30:43 +0530

    signup page implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,11 @@
 
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import path from "path";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 
@@ -15,6 +16,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); // req.body
+app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
